304d9a50eb889ba208e523959519e7af170ccfb9
davemerritt <davidmerritt6@gmail.com>
2013-11-08 14:09:39 -0500

    Now to do sql things

--- photogur/config/routes.rb
+++ photogur/config/routes.rb
@@ -1,5 +1,8 @@
 Photogur::Application.routes.draw do
+  resources :pictures
+  root :to => "pictures#index"  
   # The priority is based upon order of creation: first created -> highest priority.
+
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
